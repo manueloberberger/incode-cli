a785ed497e42ba0b80c752d65e4e10100feb7a2d
manueloberberger <manuel.oberberger@icloud.com>
2026-01-18 14:52:00 +0100

    chore: bump version to v2.4.8

--- src/config.py
+++ src/config.py
@@ -23,7 +23,7 @@ theme = Theme({
 
 console = Console(theme=theme)
 
-VERSION = "2.4.7"
+VERSION = "2.4.8"
 
 BANNER = rf"""
  [bold red]  ___ _  _  ___  ___  ___  ___       ___ _    ___   [/bold red] 
