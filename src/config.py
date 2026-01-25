c44c7d43c63e7560ecfd30b4798be4a95d044bca
manueloberberger <manuel.oberberger@icloud.com>
2026-01-25 22:34:52 +0100

    fix: Restore 'Urlaub' label on holidays (v2.16.4)
    - Modified api_async.py to check if 'urlaub' is in the reason before overriding with holiday label
    - Bumped version to 2.16.4

--- src/config.py
+++ src/config.py
@@ -38,7 +38,7 @@ theme = Theme({
 
 console = Console(theme=theme)
 
-VERSION = "2.16.3"
+VERSION = "2.16.4"
 
 BANNER = rf"""
 [bold red]  ___ _  _  ___  ___  ___  ___       ___ _    ___   [/bold red]
